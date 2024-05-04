bca84f11df7c61582d58ea749f93d80d78e8dfec
Gene Huang <hjing643@gmail.com>
2024-05-04 23:10:46 +0100

    test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main()
 {
     printf("%s 向你问好!\n", "baseTool");
     std::cout << "test1";
-    int itest = baseTool::stringConversion::StringToT<int>("12345");
+    int itest = baseTool::stringConversion::StringToT<int>("123456");
     return 0;
 }
